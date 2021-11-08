d091eab4e5f47cdb8bdf552f6afb41ec10568ee4
Alexis <alexisvedrenne482@gmail.com>
2021-11-08 13:38:29 +0100

    Mise en place de la base de donnée et début de la page index

---
+++ src/Controller/TestController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+* @Route("/test", name="test_")
+*/
+class TestController extends AbstractController
+{
+
+    /**
+     * @Route("",name="")
+     */
+    public function index(): Response
+    {
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'Le couz',
+        ]);
+    }
+}
